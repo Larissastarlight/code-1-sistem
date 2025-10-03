5704ac7b2f7b76444927ada936b41bfda1505b6d
Larissastarlight <alves.silva.larissa2005@escola.pr.gov.br>
2025-10-03 07:43:44 -0300

    Add files via upload

---
+++ sequencia-ecf.py
@@ -0,0 +1,8 @@
+print("MINI QUESTIONARIO")
+nome= input ("qual seu nome?\n")
+idade= input("Qual a sua idade?\n")
+genero= input("Voce eh homem ou mulher?\n")
+endereco= input("Qual o seu endereco?\n")
+
+
+print("Ola,", nome, "!")

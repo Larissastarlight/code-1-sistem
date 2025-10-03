be78c4c6a04a046d9c67c901489f308512e670e0
Larissastarlight <alves.silva.larissa2005@escola.pr.gov.br>
2025-10-03 07:59:29 -0300

    Add files via upload

--- sequencia-ecf.py
+++ sequencia-ecf.py
@@ -1,8 +1,15 @@
-print("MINI QUESTIONARIO")
-nome= input ("qual seu nome?\n")
-idade= input("Qual a sua idade?\n")
-genero= input("Voce eh homem ou mulher?\n")
-endereco= input("Qual o seu endereco?\n")
+import os
+import sys
 
+print("MINI QUESTIONARIO\n")
+nome = input("Qual seu nome?\n")
+idade = input("Qual a sua idade?\n")
+genero = input("Você é homem ou mulher?\n")
+endereco = input("Qual o seu endereco?\n")
 
-print("Ola,", nome, "!")
+os.system('cls')
+
+print("Olá, ", nome, "\n")
+print("idade: ", idade, "\n")
+print("genero: ", genero, "\n")
+print("endereco: ", endereco, "\n")
